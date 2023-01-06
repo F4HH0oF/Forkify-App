dcaac963c4b4c1037875049a24183a769aa3a6cc
F4HH0oF <alekzanderignatov@gmail.com>
2023-01-06 10:46:39 +0200

    End of forkify course

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2c50078d-5569-404a-874c-c7847550f4d3';
+export const MODAL_CLOSE_SEC = 2.5;
